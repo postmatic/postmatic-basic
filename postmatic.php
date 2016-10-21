3fcd0d5cf39721f515264a307d0f50208ca3742c
Dylan Kuhn <dylan.k.kuhn@gmail.com>
2016-10-20 19:50:33 -0700

    Use composer autoloader

--- postmatic.php
+++ postmatic.php
@@ -32,18 +32,7 @@ if ( class_exists( 'Prompt_Core' ) ) {
 	return;
 }
 
-require_once dirname( __FILE__ ) . '/vendor/scribu/scb-framework/load.php';
 require_once dirname( __FILE__ ) . '/core/autoload.php';
-
-Prompt_Autoload::register( 'Prompt_', dirname( __FILE__ ) . '/core' );
-Prompt_Autoload::register( 'Prompt_', dirname( __FILE__ ) . '/core/commands', '_Command' );
-Prompt_Autoload::register( 'Prompt_', dirname( __FILE__ ) . '/core/mailers', '_Mailer' );
-Prompt_Autoload::register( 'Prompt_', dirname( __FILE__ ) . '/core/email-batches', '_Email_Batch' );
-Prompt_Autoload::register( 'Prompt_', dirname( __FILE__ ) . '/core/matchers', '_Matcher' );
-Prompt_Autoload::register( 'Prompt_', dirname( __FILE__ ) . '/core/post-rendering-modifiers', '_Post_Rendering_Modifier' );
-Prompt_Autoload::register( 'Prompt_Interface_', dirname( __FILE__ ) . '/interfaces' );
-Prompt_Autoload::register( 'Prompt_Admin_', dirname( __FILE__ ) . '/admin' );
-Prompt_Autoload::register( 'Prompt_Enum_', dirname( __FILE__ ) . '/enums' );
 
 Prompt_Core::load();
 
