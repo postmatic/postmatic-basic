a38ba7a91661f09b2a7fd06b178819a8bfe4350d
Jason Lemieux <jason@gopostmatic.com>
2016-11-21 18:15:18 +0000

    marketing language

--- templates/core-options-promo.php
+++ templates/core-options-promo.php
@@ -4,7 +4,9 @@
  */
 ?>
 <div id="core-options-promo">
-	<h2><?php _e( '<span>Postmatic Basic is only part of the story</span>', 'Postmatic' ); ?></h2>
-	<p>Upgrade Postmatic and tap into everything your community has to offer. <br />Build community with powerful features like guaranteed mail delivery, post and comment digests, contextual comment notifications, analytics, and monetization.</p>
-	<p><a href="http://gopostmatic.com/features" target="_blank" class="btn-regular btn-postmatic">Learn More</a></p>
+	<h2><?php _e( '<span>Email was never meant to be a one-way street.</span>', 'Postmatic' ); ?></h2>
+	<p>Ever gotten a comment notification and just wanted to hit reply? So have your readers.</p>
+	<p>Enable two-way comment notifications for just $2.99 a month. Support a fantastic WordPress startup at the same time.</p>
+	<p><a href="o/wp-admin/options-general.php?page=postmatic-pricing" target="_blank" class="btn-regular btn-postmatic">Upgrade</a></p>
 </div>
+ 
