526acbd5b43241eb6b25ba36695dc3a796c7e597
Jason Lemieux <jason@gopostmatic.com>
2016-11-23 04:01:54 +0000

    upgrade tab

--- templates/core-options-promo.php
+++ templates/core-options-promo.php
@@ -5,8 +5,8 @@
 ?>
 <div id="core-options-promo">
 	<h2><?php _e( '<span>Email was never meant to be a one-way street.</span>', 'Postmatic' ); ?></h2>
-	<p>Have you ever received a comment notification and just wanted to hit reply? No forms. No browser. Just email.<br />
-	<em>Your readers have</em>.</p>
+	<p>Have you ever received a comment notification and just wanted to hit reply? No forms, no browser, just email?<br />
+	<em>We bet your readers have, too.</em>.</p>
 	<p>Have better conversations, improved SEO, and support a fantastic WordPress startup at the same time.<br />Enable two-way comment notifications for just $2.99 a month. The first month is free.</p>
 	<p><a href="o/wp-admin/options-general.php?page=postmatic-pricing" target="_blank" class="btn-regular btn-postmatic">Upgrade</a></p>
 </div>
