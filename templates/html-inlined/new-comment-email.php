c8989ea15a0406cda41d24723325833728fc4267
Dylan Kuhn <dylan.k.kuhn@gmail.com>
2016-12-28 14:06:30 -0800

    Remove redundant content, fixes #74, see also #41

--- templates/html-inlined/new-comment-email.php
+++ templates/html-inlined/new-comment-email.php
@@ -14,47 +14,11 @@
  * @var bool                $is_api_delivery
  * @var string              $post_author_message
  */
-
-$previous_index = count( $previous_comments );
 ?>
 <div class="padded" style="margin: 0; font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 100%; line-height: 1.6; padding: 0 20px 20px 20px;">
 
 	<p class="padding" style="margin: 0; font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; line-height: 1.6; font-size: 14px; margin-bottom: 10px; font-weight: normal; padding: 10px 0;">{{{subscriber_comment_intro_html}}}</p>
 
-	<div class="new-reply" style="margin: 0; padding: 0; font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 100%; line-height: 1.6;">
-		<div class="primary-comment comment" style="margin: 0; padding: 0; font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 100%; line-height: normal; min-height: 55px; clear: left;">
-			<div class="comment-header" style="margin: 0; padding: 0; font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; line-height: 1.6; font-size: 100%; padding-bottom: 0; margin-bottom: 20px;">
-				<?php echo $avatar; ?>
-				<div class="author-name" style="margin: 0; padding: 0; font-size: 100%; font-family: serif; line-height: normal; display: inline; margin-left: 12px; font-style: italic;">
-					<?php if ( $comment_author_url ) : ?>
-						<a href="<?php echo esc_url( $comment_author_url ); ?>" style="margin: 0; padding: 0; font-size: 100%; color: #404040; font-family: serif; line-height: normal; font-style: italic;">
-							<?php echo $commenter_name; ?>
-						</a>
-					<?php else : ?>
-						<?php echo $commenter_name; ?>
-					<?php endif; ?>
-				</div>
-				<div class="comment-body" style="margin: 0; padding: 0; font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 100%; line-height: 1.6; margin-left: 60px; color: #000;">
-					<?php echo $comment_text; ?>
-				</div>
-			</div>
-		</div>
-	</div>
-
-	<div class="reply-prompt" style="margin: 0; padding: 0; font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 100%; line-height: 1.6; clear: both; margin-top: 0px; margin-bottom: 20px;">
-		<img src="<?php echo Prompt_Core::$url_path . '/media/reply-comment-2x.png'; ?>" width="30" height="30" align="left" style="margin: 0; padding: 0; font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 100%; line-height: 1.6; outline: none; display: block; max-width: 100%; text-decoration: none; -ms-interpolation-mode: bicubic; width: 30px; height: 30px; margin-right: 10px; float: left;">
-
-		<h3 class="reply" style="margin: 0; font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; padding: 0; margin-top: 15px; line-height: 1.2; font-weight: 200; margin-bottom: 15px; padding-bottom: 15px; margin-left: 20px; font-size: 100%; padding-top: 5px; clear: none;">
-			<a href="mailto:{{{reply_to}}}?subject=<?php
-				echo rawurlencode( sprintf( __( 'In reply to %s', 'Postmatic' ), $commenter_name ) );
-				?>" style="margin: 0; padding: 0; font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 100%; line-height: 1.6; color: #404040; text-decoration: none;"><?php printf( __( 'Reply to this email to reply to %s.', 'Postmatic' ), $commenter_name ); ?>
-			</a>
-		</h3>
-	</div>
-</div>
-
-
-<div class="padded" style="margin: 0; font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 100%; line-height: 1.6; padding: 0 20px 20px 20px;">
 	<p style="margin: 0; padding: 0; font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; line-height: 1.6; font-size: 14px; margin-bottom: 10px; font-weight: normal;">
 		<?php
 		/* translators: %1$s is commenter name, %2$s is post title */
