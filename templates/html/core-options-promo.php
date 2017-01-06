4da58fc938fb37e4540d7c8a7b8aa503cb3ac8e8
Jason Lemieux <jason@gopostmatic.com>
2017-01-06 03:31:58 +0000

    interface improvements

--- templates/html/core-options-promo.php
+++ templates/html/core-options-promo.php
@@ -13,8 +13,7 @@
 	<div id="core-options-promo" class="goupgrade">
 		<h2><?php _e( '<span>Email was never meant to be a one-way street.</span>', 'Postmatic' ); ?></h2>
 		<p>Have you ever received a comment notification and just wanted to hit reply? No forms, no browser, just email?<br />
-		<em>We bet your readers have, too.</em>.</p>
-		<p>Have better conversations, improved SEO, and support a fantastic WordPress startup at the same time.<br />Enable two-way comment notifications for just $2.99 a month. The first month is free.</p>
+		<p>Have better conversations, improve SEO, and support a fantastic WordPress startup at the same time.<br />Enable two-way comment notifications for just $2.99 a month. The first month is free.</p>
 		<p><a href="<?php echo admin_url( 'options-general.php?page=postmatic-pricing&trial=true' ); ?>" class="btn-regular btn-postmatic">Upgrade</a></p>
 	</div>
 	<?php elseif ( $is_trial_underway and $is_key_present ) : ?>
