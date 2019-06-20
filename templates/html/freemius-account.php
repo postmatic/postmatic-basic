0e3da88834a2a2617b4ab38850d22fa8d80416a2
Ronald Huereca <ronald@mediaron.com>
2019-06-19 21:24:22 -0500

    Adding tabs to Freemius account page.

--- templates/html/freemius-account.php
+++ templates/html/freemius-account.php
@@ -6,52 +6,6 @@
  */
 ?>
 <style>
-ul.ui-tabs-nav {
-	margin: 25px 0 35px 0;
-	padding: 0;
-	display: flex;
-	flex-wrap: wrap;
-	justify-content: left;
-	list-style: none;
-	border-bottom: 1px solid #4A4A4A;
-}
-
-ul.ui-tabs-nav li {
-	margin: 0;
-}
-
-ul.ui-tabs-nav a {
-	padding: 0.5rem 0.75rem;
-	font-size: 80%;
-	font-weight: bold;
-	text-decoration: none;
-	display: flex;
-	align-items: flex-start;
-	color: #fff;
-	background:#4A4A4A;
-	margin: -4px 1px 0 0;
-	-webkit-border-top-left-radius: 5px;
-	-webkit-border-top-right-radius: 15px;
-	-moz-border-radius-topleft: 5px;
-	-moz-border-radius-topright: 15px;
-	border-top-left-radius: 5px;
-	border-top-right-radius: 15px;
-	border-top: 1px solid #23282D;
-}
-ul.ui-tabs-nav li.ui-state-active a,ul.ui-tabs-nav li a:hover {
-	background: #43C0C7;
-	border-top: 1px solid #43C0C7;
-}
-
-ul.ui-tabs-nav li#prompt-tab-upgrade a {
-	background: #D54E21;
-	border: 1px solid #FA5923;
-}
-
-ul.ui-tabs-nav li#prompt-tab-upgrade.ui-state-active a {
-	background: #D54E21;
-	border: 1px solid #FA5923;
-}
 h2#prompt-settings-header {
 	background: url(<?php echo esc_url( Prompt_Core::$url_path ); ?>/media/replyable.png) no-repeat top left;
 	background-size: 250px;
@@ -73,6 +27,7 @@ h2#prompt-settings-header span {
 	<h2 id="prompt-settings-header"><span>Replyable</span></h2>
 
 
-<ul class="ui-tabs-nav ui-helper-reset ui-helper-clearfix ui-widget-header ui-corner-all" role="tablist"><li id="prompt-tab-core" class="ui-state-default ui-corner-left" style="" role="tab" tabindex="0" aria-controls="prompt-settings-core" aria-labelledby="ui-id-1" aria-expanded="true"><a href="/wp-admin/options-general.php?page=postmatic" class="ui-tabs-anchor" role="presentation" tabindex="-1" id="ui-id-1">Get Started</a></li><li id="prompt-tab-configure-your-template" class="ui-state-default ui-corner-left" style="" role="tab" tabindex="-1" aria-controls="prompt-settings-configure-your-template" aria-labelledby="ui-id-2" aria-selected="false" aria-expanded="false"><a href="/wp-admin/options-general.php?page=postmatic#prompt-settings-configure-your-template" class="ui-tabs-anchor" role="presentation" tabindex="-1" id="ui-id-2">Configure Your Template</a></li><li id="prompt-tab-comment-delivery" class="ui-state-default ui-corner-left" style="" role="tab" tabindex="-1" aria-controls="prompt-settings-comment-delivery" aria-labelledby="ui-id-3" aria-selected="false" aria-expanded="false"><a href="/wp-admin/options-general.php?page=postmatic#prompt-settings-comment-delivery" class="ui-tabs-anchor" role="presentation" tabindex="-1" id="ui-id-3">Comment Subscription Options</a></li><li id="prompt-tab-import-subscribe-reloaded" class="ui-state-default ui-corner-left" style="" role="tab" tabindex="-1" aria-controls="prompt-settings-import-subscribe-reloaded" aria-labelledby="ui-id-4" aria-selected="false" aria-expanded="false"><a href="/wp-admin/options-general.php?page=postmatic#prompt-settings-import-subscribe-reloaded" class="ui-tabs-anchor" role="presentation" tabindex="-1" id="ui-id-4">Importer</a></li><li id="prompt-tab-recommended-plugins" class="ui-state-default ui-corner-left" style="" role="tab" tabindex="-1" aria-controls="prompt-settings-recommended-plugins" aria-labelledby="ui-id-5" aria-selected="false" aria-expanded="false"><a href="/wp-admin/options-general.php?page=postmatic#prompt-settings-recommended-plugins" class="ui-tabs-anchor" role="presentation" tabindex="-1" id="ui-id-5">Recommended Plugins</a></li></ul>
+	<h2 class="nav-tab-wrapper">
+		<a href="<?php echo esc_url( admin_url( 'options-general.php?page=postmatic&tab=prompt-tab-content-core' ) ); ?>" id="prompt-tab-core" class="nav-tab show nav-tab-active" data-tab-name="prompt-tab-content-core" style=""><?php esc_html_e( 'Get Started', 'postmatic' ); ?></a><a href="<?php echo esc_url( admin_url( 'options-general.php?page=postmatic&tab=prompt-tab-content-configure-your-template' ) ); ?>" id="prompt-tab-configure-your-template" class="nav-tab" data-tab-name="prompt-tab-content-configure-your-template" style=""><?php esc_html_e( 'Configure Your Template', 'postmatic' ); ?></a><a href="<?php echo esc_url( admin_url( 'options-general.php?page=postmatic&tab=prompt-tab-content-comment-delivery' ) ); ?>" id="prompt-tab-comment-delivery" class="nav-tab" data-tab-name="prompt-tab-content-comment-delivery" style=""><?php esc_html_e( 'Comment Subscription Options', 'postmatic' ); ?></a><a href="<?php echo esc_url( admin_url( 'options-general.php?page=postmatic&tab=prompt-tab-content-import-subscribe-reloaded' ) ); ?>" id="prompt-tab-import-subscribe-reloaded" class="nav-tab" data-tab-name="prompt-tab-content-import-subscribe-reloaded" style=""><?php echo esc_html_e( 'Importer', 'postmatic' ); ?></a><a href="<?php echo esc_url( admin_url( 'options-general.php?page=postmatic&tab=prompt-tab-content-recommended-plugins' ) ); ?>" id="prompt-tab-recommended-plugins" class="nav-tab" data-tab-name="prompt-tab-content-recommended-plugins" style=""><?php esc_html_e( 'Recommended Plugins', 'postmatic' ); ?></a></h2>
 <?php echo $account_html; ?>
 </div>
